449d7b9f753c7862e2a4dce4882d85b4be9dd965
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2016-09-25 11:12:30 +0300

    starting coding work with VS2015

---
+++ Assets/Scripts/Const/EntityKeys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TankArena.Constants
+{
+    class EntityKeys
+    {
+        public const String EK_NAME = "name";
+        public const String EK_ID = "id";
+    }
+}
---
+++ Assets/Scripts/Models/FileLoadedEntityModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TankArena.Constants;
+
+namespace TankArena.Models
+{
+    abstract class FileLoadedEntityModel
+    {
+        public String Id
+        {
+            get
+            {
+                return (string)properties[EntityKeys.EK_ID];
+            }
+        }
+
+        public String Name
+        {
+            get
+            {
+                return (string)properties[EntityKeys.EK_NAME];
+            }
+        }
+
+        protected Dictionary<String, Object> properties;
+
+        public FileLoadedEntityModel(string filePath)
+        {
+            LoadPropertiesFromJSON(filePath);
+        }
+
+        private void LoadPropertiesFromJSON(string filePath)
+        {
+
+        }
+    }
+}
