eb813c0cc1f0402fb634949f5fb3b2c6795e6527
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2016-09-27 22:49:18 +0300

    Adding component creation via generators from JSON base paths

---
+++ Assets/Scripts/Utils/EntitiesLoaderUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using TankArena.Models;
+using UnityEngine;
+
+namespace TankArena.Utils
+{
+    class EntitiesLoaderUtil
+    {
+        private EntitiesLoaderUtil() { }
+
+        public static void loadAllEntitesAtPath<T>(string basePath, Func<string, T> generator, Dictionary<String, T> consumer) 
+            where T : FileLoadedEntityModel
+        {
+            if (consumer == null) { consumer = new Dictionary<string, T>(); };
+            foreach (String fileName in Directory.GetFiles(basePath, "*.json", SearchOption.AllDirectories))
+            {
+                var entity = generator(fileName);
+                consumer.Add(entity.Id, entity);
+            }
+            Debug.Log(String.Format("Loaded {0} entites of type {1}", consumer.Count, typeof(T).FullName));
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/EntitiesStore.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using TankArena.Models;
+using TankArena.Models.Tank;
+using TankArena.Models.Tank.Weapons;
+using TankArena.Models.Characters;
+
+namespace TankArena.Utils
+{
+    class EntitiesStore : Singleton<EntitiesStore>
+    {
+        protected EntitiesStore() { }
+
+        public Dictionary<String, FileLoadedEntityModel> Entities { get { return loadedEntities; }  }
+        public Dictionary<String, PlayableCharacter> Characters { get { return loadedCharacters; } }
+        public Dictionary<String, TankPart> TankParts { get { return loadedTankParts; } }
+        public Dictionary<String, BaseWeapon> Weapons { get { return loadedWeapons; } }
+
+        private Dictionary<String, FileLoadedEntityModel> loadedEntities;
+        private Dictionary<String, PlayableCharacter> loadedCharacters;
+        private Dictionary<String, TankPart> loadedTankParts;
+        private Dictionary<String, BaseWeapon> loadedWeapons;
+
+        public void Awake()
+        {
+            loadedEntities = new Dictionary<string, FileLoadedEntityModel>();
+
+            EntitiesLoaderUtil.loadAllEntitesAtPath<PlayableCharacter>("", path => { return new PlayableCharacter(path); }, loadedCharacters);
+            loadedCharacters.ToList().ForEach(x => loadedEntities.Add(x.Key, x.Value));
+            
+        }
+
+        public void GetStatus()
+        {
+            //print amounts of loaded entities
+            //as status info. Good for eager lazy loading
+            Debug.Log(String.Format("Loaded total of {0} entities.\n", loadedEntities.Count));
+        }
+
+       
+    }
+}
