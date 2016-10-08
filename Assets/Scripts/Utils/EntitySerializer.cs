350caac05cdb6c403f3036fb04f9418e0865a5ea
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2016-10-09 01:35:09 +0300

    Starting work on item deserialization

--- Assets/Scripts/Utils/EntitySerializer.cs
+++ Assets/Scripts/Utils/EntitySerializer.cs
@@ -12,23 +12,38 @@ namespace TankArena.Utils
 {
     class EntitySerializer
     {
-        private static Func<FileLoadedEntityModel, String> convertNameVal = entity => String.Format("{0}={1}", entity.EntityKey, entity.Id);
-        private static Func<FileLoadedEntityModel, String> convertJustId = entity => entity.Id;
+        private static Func<FileLoadedEntityModel, String> convertToNameVal = entity => String.Format("{0}={1}", entity.EntityKey, entity.Id);
+        private static Func<FileLoadedEntityModel, String> convertToJustId = entity => entity.Id;
+        private static Func<String, FileLoadedEntityModel> convertFromNameVal = code => EntitiesStore.Instance.Entities[code.Split(new[] { '=' })[1]];
+        private static Func<String, FileLoadedEntityModel> convertFromJustId = code => EntitiesStore.Instance.Entities[code];
         private static Dictionary<Type, Func<FileLoadedEntityModel, String>> serializers;
-        
+        private static Dictionary<Type, Func<String, FileLoadedEntityModel>> deserializers;
 
         static EntitySerializer()
         {
+            InitSerializers();
+            InitDeserializers();
+        }
+
+        private static void InitDeserializers()
+        {
+            deserializers = new Dictionary<Type, Func<string, FileLoadedEntityModel>>();
+
+
+        }
+
+        private static void InitSerializers()
+        {
             serializers = new Dictionary<Type, Func<FileLoadedEntityModel, string>>();
-            serializers.Add(typeof(FileLoadedEntityModel), convertNameVal);
-            serializers.Add(typeof(PlayableCharacter), convertJustId);
-            serializers.Add(typeof(TankPart), convertNameVal);
-            serializers.Add(typeof(TankEngine), convertNameVal);
-            serializers.Add(typeof(TankTracks), convertNameVal);
+            serializers.Add(typeof(FileLoadedEntityModel), convertToNameVal);
+            serializers.Add(typeof(PlayableCharacter), convertToJustId);
+            serializers.Add(typeof(TankPart), convertToNameVal);
+            serializers.Add(typeof(TankEngine), convertToNameVal);
+            serializers.Add(typeof(TankTracks), convertToNameVal);
             serializers.Add(typeof(TankChassis), entity =>
             {
-                TankChassis chassis = (TankChassis) entity;
-                var chassisCode = convertNameVal(chassis);
+                TankChassis chassis = (TankChassis)entity;
+                var chassisCode = convertToNameVal(chassis);
                 StringBuilder codeBuilder = new StringBuilder(chassisCode);
                 codeBuilder.Append(",").Append(EntitySerializer.SerializeEntity(chassis.Engine));
                 codeBuilder.Append(",").Append(EntitySerializer.SerializeEntity(chassis.Tracks));
@@ -38,8 +53,8 @@ namespace TankArena.Utils
             });
             serializers.Add(typeof(TankTurret), entity =>
             {
-                TankTurret turret = (TankTurret) entity;
-                var turretString = convertNameVal(turret);
+                TankTurret turret = (TankTurret)entity;
+                var turretString = convertToNameVal(turret);
                 //weapon slots : H_0 - first heavy, L_1: second light, etc
                 //this numbering is consistent because the slots themselves are an 
                 //ordered json list
@@ -59,7 +74,7 @@ namespace TankArena.Utils
 
                 return codeBuilder.ToString();
             });
-            serializers.Add(typeof(BaseWeapon), convertJustId);
+            serializers.Add(typeof(BaseWeapon), convertToJustId);
         }
 
 
@@ -86,8 +101,25 @@ namespace TankArena.Utils
 
         public static T DeserializeEntity<T>(String code) where T: FileLoadedEntityModel
         {
+            if (!String.IsNullOrEmpty(code))
+            {
+                //has direct deserializers, return that
+                if (deserializers.ContainsKey(typeof(T)))
+                {
+                    return (T)deserializers[typeof(T)](code);
+                }
+
+                foreach (var attempt in deserializers)
+                {
+                    if (typeof(T).IsAssignableFrom(attempt.Key))
+                    {
+                        return (T)attempt.Value(code);
+                    }
+                }
+            }
             return null;
         }
+
 
     }
 }
