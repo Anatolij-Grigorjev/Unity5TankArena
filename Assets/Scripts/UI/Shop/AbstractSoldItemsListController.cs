910cb694fba28930d71538d4ff4b8e03d9dc283a
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2016-12-23 00:58:49 +0200

    start of work on sold items controller

--- Assets/Scripts/UI/Shop/AbstractSoldItemsListController.cs
+++ Assets/Scripts/UI/Shop/AbstractSoldItemsListController.cs
@@ -7,14 +7,29 @@ using TankArena.Models;
 public class AbstractSoldItemsListController<T> : MonoBehaviour where T: FileLoadedEntityModel
 {
 
-	List<T> items;
+	private List<T> items;
 
+	private List<GameObject> visibleObjectsList;
 	
 
 	// Use this for initialization
 	void Start () 
 	{
-	
+		UpdateList();
+	}
+
+	public void SetItems(List<T> items) 
+	{
+		this.items = items;
+		UpdateList();
+	}
+
+	private void UpdateList()
+	{
+		//too costy to remove and add items, 
+		//update list as we go along, remove non listed items later
+		//all items need to be a common GO extension that has associated model id in it to 
+		//prove things (model id could be in GO tag?!)
 	}
 	
 	// Update is called once per frame
