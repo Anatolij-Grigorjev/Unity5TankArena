f8cd8a3fb10887bc80b3212014d3a3fecd56218a
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2016-12-23 22:03:16 +0200

    adding implementation controller

---
+++ Assets/Scripts/UI/Shop/Weapons/SoldWeaponsListController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using TankArena.UI.Shop;
+using TankArena.Models.Weapons;
+using TankArena.Constants;
+using System;
+
+public class SoldWeaponsListController: AbstractSoldItemsListController<BaseWeapon>
+{
+
+	public GameObject heavyItemPrefab;
+	public GameObject lightItemPrefab;
+
+    public override GameObject MakeGOForItem(BaseWeapon item)
+    {
+        if (item != null )
+		{
+			return Instantiate(
+				item.Type == WeaponTypes.HEAVY? heavyItemPrefab : lightItemPrefab,
+				Vector3.zero,
+				Quaternion.identity,
+				parentContainer
+			) as GameObject;
+		} else 
+		{
+			return null;
+		}
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
