d6e0ce103af0c5b5beb2cf4e087b4f075d7b215d
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2016-12-23 21:51:09 +0200

    coding work on generic shop item controller

---
+++ Assets/Scripts/Constants/UI/UIShopTiming.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TankArena.Constants
+{
+	public class UIShopTiming 
+	{
+		public static readonly float SHOP_ITEM_FADEAWAY_SEC = 1.5f;
+	}
+}
+
--- Assets/Scripts/UI/Shop/AbstractSoldItemsListController.cs
+++ Assets/Scripts/UI/Shop/AbstractSoldItemsListController.cs
@@ -3,33 +3,82 @@ using UnityEngine.UI;
 using System.Linq;
 using System.Collections.Generic;
 using TankArena.Models;
+using TankArena.Constants;
 
-public class AbstractSoldItemsListController<T> : MonoBehaviour where T: FileLoadedEntityModel
+public abstract class AbstractSoldItemsListController<T> : MonoBehaviour where T: FileLoadedEntityModel
 {
-
-	private List<T> items;
-
-	private List<GameObject> visibleObjectsList;
+	private Dictionary<T, GameObject> visibleObjectsMap;
 	
+	public Transform parentContainer;
 
 	// Use this for initialization
 	void Start () 
 	{
-		UpdateList();
+		visibleObjectsMap = new Dictionary<T, GameObject>();
+		UpdateListView();
 	}
 
 	public void SetItems(List<T> items) 
 	{
-		this.items = items;
-		UpdateList();
+		//add the GO n stuff to the data
+		foreach(T item in items) 
+		{
+			AddItem(item);
+		}
+
+		//refresh the actual content pane
+		UpdateListView();
 	}
 
-	private void UpdateList()
+	//Add item with required GO
+	public void AddItem(T item)
 	{
-		//too costy to remove and add items, 
-		//update list as we go along, remove non listed items later
-		//all items need to be a common GO extension that has associated model id in it to 
-		//prove things (model id could be in GO tag?!)
+		//no need to do anything if item already there
+		if (!ItemInList(item))
+		{
+			visibleObjectsMap[item] = MakeGOForItem(item);
+		}
+	}
+
+	//remove item and required GO
+	public void RemoveItem(T item) 
+	{
+		if (ItemInList(item))
+		{
+			var go = visibleObjectsMap[item];
+			Destroy(go, UIShopTiming.SHOP_ITEM_FADEAWAY_SEC);
+		}
+	}
+
+	//update how it looks onscreen
+	private void UpdateListView()
+	{
+		foreach (T item in visibleObjectsMap.Keys) 
+		{
+			if (!ItemInList(item))
+			{
+				visibleObjectsMap[item] = MakeGOForItem(item);
+			}		
+		}
+
+	}
+
+	public abstract GameObject MakeGOForItem(T item);
+
+	//key and GO are present
+	private bool ItemInList(T item)
+	{
+		if (item == null || string.IsNullOrEmpty(item.Id)) 
+		{
+			return false;
+		}
+		if (visibleObjectsMap == null || visibleObjectsMap.Count == 0)
+		{
+			return false;
+		}
+		return visibleObjectsMap.ContainsKey(item) 
+			&& visibleObjectsMap[item] != null
+			&& visibleObjectsMap[item].transform.parent == parentContainer;
 	}
 	
 	// Update is called once per frame
