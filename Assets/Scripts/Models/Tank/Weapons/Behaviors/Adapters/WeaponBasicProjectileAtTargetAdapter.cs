4eebdc5a9bea8f5903fb6f2def0243fa60cc2550
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2017-01-23 21:08:54 +0200

    fixing the no-collision bug

--- Assets/Scripts/Models/Tank/Weapons/Behaviors/Adapters/WeaponBasicProjectileAtTargetAdapter.cs
+++ Assets/Scripts/Models/Tank/Weapons/Behaviors/Adapters/WeaponBasicProjectileAtTargetAdapter.cs
@@ -16,26 +16,23 @@ namespace TankArena.Models.Weapons.Behaviors
             DBG.Log("Shot time position: {0}, up: {1}", shotTimePosition, shotTimeUp);
             bool didHit = false;
             DBG.Log("Feeder: {0}", controller.gameObject);
-            int count = Physics2D.CircleCastNonAlloc(
+            RaycastHit2D firstHit = Physics2D.CircleCast(
                 shotTimePosition, 
-                projectileRadius, 
+                5.0f,
                 shotTimeUp,
-                hitsNonAlloc,
                 weapon.Range,
                 layerMask
             );
-            didHit = count > 0;
-            DBG.Log("Got results: {0}, from firing {1}", count, weapon.Name);
+            didHit = firstHit.collider != null;
             Vector3 pos = Vector3.zero;
-            DBG.Log("Weapon Layer mask: {0}, default and enemy layer mask: {1}", layerMask, LayerMasks.LM_DEFAULT_AND_ENEMY);
             if (!didHit)
             {
                 pos = shotTimePosition + (weapon.Range * shotTimeUp);
             }
             else 
             {
-                DBG.Log("Hit collider of GO: {0}", hitsNonAlloc[0].collider.gameObject);
-                pos = new Vector3(hitsNonAlloc[0].point.x, hitsNonAlloc[0].point.y);
+                DBG.Log("Hit collider of GO: {0}", firstHit.collider.gameObject);
+                pos = new Vector3(firstHit.point.x, firstHit.point.y);
             }
 
             CreateAndConfigureProjectile(didHit, pos);
