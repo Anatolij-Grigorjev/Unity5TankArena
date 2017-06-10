e3452f4e61c661b0438a8a360d543bd550bbe78a
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2017-06-10 21:39:24 +0300

    trying to construct importer command

--- Assets/Editor/SpriteImporter.cs
+++ Assets/Editor/SpriteImporter.cs
@@ -1,11 +1,81 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class SpriteImporter : MonoBehaviour {
+public class SpriteImporter : MonoBehaviour
+{
 
-	public static void ImportSprite() 
-	{
-		
-	}
+    public static void ImportSprite()
+    {
+        var commandLineArgsMap = makeArgsMap();
+
+        importSpriteSheet(commandLineArgsMap);
+    }
+
+    private static void importSpriteSheet(Dictionary<string, string> commandLineArgsMap)
+    {
+        var spritePath = commandLineArgsMap["-spritesheet"];
+        var spriteWidth = int.Parse(commandLineArgsMap["-width"]);
+        var spriteHeight = int.Parse(commandLineArgsMap["-height"]);
+        var rows = int.Parse(commandLineArgsMap["-rows"]);
+        var cols = int.Parse(commandLineArgsMap["-cols"]);
+        var paddingX = commandLineArgsMap.ContainsKey("paddingX") ?
+            int.Parse(commandLineArgsMap["-paddingX"]) : 0;
+        var paddingY = commandLineArgsMap.ContainsKey("paddingY") ?
+            int.Parse(commandLineArgsMap["paddingY"]) : 0;
+        int spritesCount = rows * cols;
+
+        var textureImporter = AssetImporter.GetAtPath(spritePath) as TextureImporter;
+        List<SpriteMetaData> metaDataList = new List<SpriteMetaData>();
+
+
+        for (int i = 0; i < rows; i++)
+		{
+            for (int j = 0; j < cols; j++)
+            {
+                var metaData = new SpriteMetaData();
+                metaData.rect = new Rect(
+					j * spriteWidth + j * paddingX,
+					i * spriteHeight + i * paddingY,
+					spriteWidth,
+					spriteHeight
+                );
+                metaData.pivot = metaData.rect.center;
+                metaDataList.Add(metaData);
+            }
+		}
+
+        textureImporter.spritesheet = metaDataList.ToArray();
+        AssetDatabase.ImportAsset(spritePath, ImportAssetOptions.ForceUpdate);
+    }
+
+    private static Dictionary<string, string> makeArgsMap()
+    {
+        var argsArray = System.Environment.GetCommandLineArgs();
+        var mapping = new Dictionary<string, string>(argsArray.Length / 2);
+        for (int i = 0; i < argsArray.Length; i++)
+        {
+            //this is a parameter
+            if (
+                (i < argsArray.Length - 1) &&
+             argsArray[i].StartsWith("-") &&
+              !argsArray[i + 1].StartsWith("-")
+              )
+            {
+                mapping.Add(argsArray[i], argsArray[i + 1]);
+            }
+            else
+            {
+                if (argsArray[i].StartsWith("-"))
+                {
+                    //add bool switches
+                    mapping.Add(argsArray[i], true.ToString());
+                }
+            }
+
+        }
+
+        return mapping;
+    }
 }
---
+++ import_cmd_constructor.bat
@@ -0,0 +1,11 @@
+"C:\Program Files\Unity\Editor\Unity.exe" ^
+-batchmode ^
+-quit ^
+-projectPath "C:\Users\Anatolij\Documents\Unity Projects\TankArena" ^
+-executeMethod SpriteImporter.ImportSprite ^
+-spritesheet "C:\Users\Anatolij\Documents\Unity Projects\TankArena\Assets\Resources\TankParts\Tracks\tracks_basic_spritesheet_0.png" ^
+-width 199 ^
+-height 199 ^
+-rows 3 ^
+-cols 4 
+
